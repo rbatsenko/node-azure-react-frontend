691fa389490caadd97a351af34e4c24a7848ddab
Roman Batsenko <rbatsenko@gmail.com>
2019-09-22 18:17:03 +0200

    Add env config and waking up request

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
 import React, { useState, useEffect } from 'react';
+import { API_URL } from './config';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
   const [planets, setPlanets] = useState([]);
   const [error, setError] = useState(null);
-  const [loading, setLoading] = useState(true);
+  const [loading, setLoading] = useState(false);
+  const [waking, setWaking] = useState(true);
 
   useEffect(() => {
-    const fetchData = async () => {
+    const fetchPlanets = async () => {
+      setLoading(true);
       try {
-        const response = await fetch('https://rbatsenko.azurewebsites.net/api/httpPlanets');
+        const response = await fetch(API_URL);
         const { data } = await response.json();
         setError(null);
         setPlanets(data);
@@ -24,6 +27,29 @@ function App() {
         setLoading(false);
       }
     }
+    const fetchData = async () => {
+      try {
+        const response = await fetch(API_URL, {
+          method: 'POST',
+          body: JSON.stringify({ wakeUp: true }),
+          headers: {
+            'Content-Type': 'application/json',
+          }
+        });
+        const json = await response.json();
+        try {
+          const { wakingUp } = json.data;
+          if (wakingUp) {
+            setWaking(false);
+            fetchPlanets();
+          }
+        } catch (err) {
+          setError(err.message);
+        }
+      } catch(error) {
+        setError(error.message);
+      }
+    }
     fetchData();
   }, []);
 
@@ -33,6 +59,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         {error && <p style={{color: "red"}}>{error}</p>}
         {loading && 'Loading...'}
+        {waking && 'Waking server up...'}
         <ul style={{padding: 0, textAlign: 'left'}}>
           {planets.length > 0 && planets.map(planet => <li key={planet.name}>{planet.name} - {planet.terrain}</li>)}
         </ul>
